c3454764b7966935caf58bdc81b4466099b9a6a9
Parker Woodard <parkerjwoodard@gmail.com>
2025-03-01 14:50:39 -0800

    just savin my spot :)

--- libraries/vulkan/src/vulkan_api.cpp
+++ libraries/vulkan/src/vulkan_api.cpp
@@ -1,11 +1,79 @@
 #include "vulkan/vulkan_api.h"
 
+#include <stdexcept>
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+namespace
+{
+    VkInstance create_vulkan_instance()
+    {
+        VkInstance instance{};
+
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+        return instance;
+    }
+}
 
 namespace deep
 {
 void VulkanApi::initialize()
 {
+    init_window();
+    init_vulkan();
+}
+
+void VulkanApi::destroy()
+{
+    glfwDestroyWindow(window_);
+    glfwTerminate();    
+}
+
+void VulkanApi::init_vulkan()
+{
+    create_vulkan_instance();
+}
+
+void VulkanApi::init_window()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window_ = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr); 
+}
+
+void VulkanApi::main_loop()
+{
+    while (!glfwWindowShouldClose(window_)) {
+        glfwPollEvents();
+    }
 }
 }
