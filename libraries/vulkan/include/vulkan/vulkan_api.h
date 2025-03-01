5c96dea78a511343d3bfbdbed999cb5a2dfb2b38
Parker Woodard <parkerjwoodard@gmail.com>
2025-03-01 14:50:39 -0800

    just savin my spot :)

--- libraries/vulkan/include/vulkan/vulkan_api.h
+++ libraries/vulkan/include/vulkan/vulkan_api.h
@@ -1,5 +1,7 @@
 #pragma once
 
+class GLFWwindow;
+
 namespace deep
 {
 
@@ -9,5 +11,12 @@ class VulkanApi
     VulkanApi();
 
     void initialize();
+    void destroy(); 
+  private:
+    void init_window();
+    void init_vulkan();
+    void main_loop();
+
+    GLFWwindow* window_;
 };
 }// namespace deep
