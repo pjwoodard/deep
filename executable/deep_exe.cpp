17dee6e8e79b41668c5fe8e79aa953e9d970f27b
Parker Woodard <parkerjwoodard@gmail.com>
2025-03-03 20:24:31 -0800

    Builds and runs an app + initializes vulkan context. Next "chapter" is validation layers

--- executable/deep_exe.cpp
+++ executable/deep_exe.cpp
@@ -1,11 +1,14 @@
-// #include "deep/deep.h"
-// #include "imgui/imgui_layer.h"
 #include "logger/logger.h"
+#include "vulkan/vulkan_api.h"
 
 using namespace std::literals;
 
 int main()
 {
     deep::Logger::info_core("Starting Vulkan Test Application");
+    deep::VulkanApi api;
+    api.initialize();
+    api.run();
+    api.destroy();
     return 0;
 }
